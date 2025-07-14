5977aa1f5d0644b29f5d332ed0bae5158e219ec9
Steve Abreu <s.abreu@rug.nl>
2025-07-14 19:47:40 +0200

    slurm script + HGRN train config

---
+++ submit-job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=hgrn_340M
+#SBATCH --partition=minor-use-case
+#SBATCH --time=2-00:00:00
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=32
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+# Activate virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
+# Load environment variables
+source .env
+
+# Print GPU status
+nvidia-smi
+
+DUMPFOLDER=exp/hgrn-340M-8K-full/batch32.seqlen65k.context8k.warmup1024.update1.steps2048000.lr3e-4.wsd
+bash train.sh \
+  --job.config_file train_configs/hgrn.toml \
+  --training.steps 204800 \
+  --training.dataset HuggingFaceTB/smollm-corpus \
+  --training.dataset_name fineweb-edu-dedup \
+  --training.dataset_split train \
+  --job.dump_folder $DUMPFOLDER \
+  --metrics.save_tb_folder $DUMPFOLDER \
+  --checkpoint.load_step -1 \
+  --checkpoint.keep_latest_k 10 \
+  --checkpoint.interval 2048
